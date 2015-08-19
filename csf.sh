20ec843adae58c6384fd8deae5d146cac8110f42
advertcn <admin@advertcn.com>
2015-08-19 10:29:10 +0800

    Create csf.sh

---
+++ csf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# this script will download csf into root directory
+cd /root
+## most of current distributions don't have libwww-perl installed by default
+yum install perl-libwww-perl
+rm -fv csf.tgz
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+sh install.sh
