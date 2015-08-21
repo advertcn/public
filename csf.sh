41be890b03b7a40f82a3931300158e87b0d6ca91
advertcn <admin@advertcn.com>
2015-08-21 17:20:30 +0800

    update for new csf version
    in the new csf version, csf needs unzip and bind-utils

--- csf.sh
+++ csf.sh
@@ -2,7 +2,7 @@
 # this script will download csf into root directory
 cd /root
 ## most of current distributions don't have libwww-perl installed by default
-yum install perl-libwww-perl -y
+yum install perl-libwww-perl unzip bind-utils -y
 rm -fv csf.tgz
 wget https://download.configserver.com/csf.tgz
 tar -xzf csf.tgz
